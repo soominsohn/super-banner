083c7aa10374b9a3ec14d5a8a1dbc78806b2bf89
soominsohn <soomin.sohn.dev@gmail.com>
2024-05-04 15:58:25 +0900

    change banner

---
+++ src/main/java/angie/near/banner/CustomBanner.java
@@ -0,0 +1,14 @@
+package angie.near.banner;
+
+import java.io.PrintStream;
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+
+public class CustomBanner implements Banner {
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+        out.println("================================================");
+        out.println("               ## Hello World  ##        ");
+        out.println("================================================");
+    }
+}
--- src/main/java/angie/near/banner/SuperBannerApplication.java
+++ src/main/java/angie/near/banner/SuperBannerApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BannerApplication {
+public class SuperBannerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BannerApplication.class, args);
+		SpringApplication application = new SpringApplication(SuperBannerApplication.class);
+		application.setBanner(new CustomBanner());
+		application.run(args);
 	}
-
 }
