68e71e38a9f0fa7c10b951e37b277006a84c1e40
soominsohn <soomin.sohn.dev@gmail.com>
2024-05-04 13:53:41 +0900

    add banner.txt

---
+++ src/main/java/angie/near/banner/BannerApplication.java
@@ -0,0 +1,13 @@
+package angie.near.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angie/near/banner/BannerApplicationTests.java
@@ -0,0 +1,13 @@
+package angie.near.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
